fcbf870745f6e00d377b1554c44acba70d33a4d5
Devon Troedel <dtroedel@atu.edu>
2018-06-21 20:16:19 -0500

    Original

--- GitHub_Tests/GitHub_Tests_1/main.cpp
+++ GitHub_Tests/GitHub_Tests_1/main.cpp
@@ -5,10 +5,4 @@
 
 int main() {
 	std::cout << "Hello World!\n";
-
-	int number{ 3 };
-
-	std::cout << "Please enter an integer: ";
-	std::cin >> number;
-	std::cout << "\nYou entered: " << number << ".\n";
 }
